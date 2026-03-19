7f15c4590138267259467c9153d88949acb41f55
Ethan Heimer <crooked@Ethans-MacBook-Pro.local>
2026-03-19 15:26:14 -0700

    Bug Fixes

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,7 @@ let filter = urlParams.get("search")
 
 filter = filter ? filter : ""
 
-console.log(window.location)
+console.log(window.location.hostname)
 
 // Create Quiz Cards
 CreateQuizCardElements(filter, (fileName, jsonBody) => {
